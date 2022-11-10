24e745d153a1211b75250b791671f36e795b7bfc
Maruf Robin <maruf.robin@northsouth.edu>
2022-11-10 09:39:43 +0600

    dependance added

---
+++ lib/db_helper/data_base_helper.dart
@@ -0,0 +1,21 @@
+import 'package:contact_list/models/model_data.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DataBaseHelper {
+
+  static const String createTableContact = '''create table $tableContact(
+  $tableContactColId integer primary key,
+  $tableContactColName text,
+  $tableContactColPhone text,
+  $tableContactColAddress text,
+  $tableContactColDob text,
+  $tableContactColGender text,
+  $tableContactColImage text)''';
+
+  static Future<Database> openDb() async {
+    final path = await getDatabasesPath();
+    final dbPath = join(path, "contact.db");
+    return openDatabase(dbPath,version: 1,onCreate: );
+  }
+}
